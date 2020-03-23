797997956843afaf143600b20abf0422d60ece2d
Tiago Sousa <tsousa95@gmail.com>
2020-03-23 17:30:06 +0000

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.destroy(params[:id])
+    flash.notice = "Tag '#{@tag.name}' was Deleted!"
+    redirect_to tags_path
+  end
+end
